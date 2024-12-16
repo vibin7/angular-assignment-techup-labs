9330898a06b2e3a9bf2aeb9089486e97ec9523c7
vibin7 <vibinthomas777@gmail.com>
2024-12-16 09:53:48 +0530

    fix: removed unused module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,25 +3,25 @@ import { LayoutComponent } from './layout/layout.component';
 
 // export const routes: Routes = [];
 export const routes: Routes = [
-    {
-      path: '', // Base path for LayoutComponent
-      component: LayoutComponent,
-      children: [
-        { path: "", redirectTo: "customer", pathMatch: "full" },
-        {
-          path: 'pins', // Route for ListComponent
-          loadComponent: () =>
-            import('./containers/pins-list/pins-list.component').then((m) => m.PinsListComponent),
-        },
-        {
-            path: 'customer', // Route for ListComponent
-            loadComponent: () =>
-              import('./containers/customer-list/customer-list.component').then((m) => m.CustomerListComponent),
-          },
-      ],
-    },
-    {
-      path: '**', // Catch-all route
-      redirectTo: 'customer',
-    },
-  ];
+  {
+    path: '', // Base path for LayoutComponent
+    component: LayoutComponent,
+    children: [
+      { path: "", redirectTo: "customer", pathMatch: "full" },
+      {
+        path: 'pins', // Route for pins list component
+        loadComponent: () =>
+          import('./containers/pins-list/pins-list.component').then((m) => m.PinsListComponent),
+      },
+      {
+        path: 'customer', // Route for customer list component
+        loadComponent: () =>
+          import('./containers/customer-list/customer-list.component').then((m) => m.CustomerListComponent),
+      },
+    ],
+  },
+  {
+    path: '**', // Catch-all route
+    redirectTo: 'customer',
+  },
+];
